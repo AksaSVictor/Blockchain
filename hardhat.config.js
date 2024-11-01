1cab33a340bf2b9adb0c2bdc3f251964e5ea04b9
Aksa <aksa.cs23@duk.ac.in>
2024-11-01 12:44:52 +0530

    :sparkles: | add contracts

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      { version: "0.8.20" },
+      { version: "0.8.22" } 
+    ],
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+};
